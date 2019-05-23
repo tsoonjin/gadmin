8c802d78bf96d2650b2003e1206e74dd76083824
jinified <jinified@gmail.com>
2019-05-23 18:44:38 +0800

    Add S3 read permissions

--- addUsers.py
+++ addUsers.py
@@ -1,3 +1,4 @@
+import json
 import boto3
 import os
 import logging
@@ -6,5 +7,11 @@ import logging
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+s3 = boto3.client('s3')
+
 def handler(event, context):
+    object = s3.get_object(Bucket='gadmin-lambda', Key='client_secrets.json')
+    serializedObject = object['Body']
+    print(serializedObject)
+    print(json.load(serializedObject))
     return {"status": 200}
